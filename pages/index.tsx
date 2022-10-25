c4f406aea24a54f447901222359232df2c56bc12
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-10-25 16:44:52 +0530

    seeding playlists

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return(
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home;
